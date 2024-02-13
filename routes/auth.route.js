e4c782b6a8d2fcab765d2dcc6f0caf0388fd95c0
Mohammad Anas <anas4u02@gmail.com>
2024-02-13 12:04:42 +0530

    Handled the updation of invitation status once the user signs up

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const User = require('../models/user.model')
 const passport = require('passport');
+const { invitationStatus } = require('../utils/constants');
 
 const { MailtrapClient } = require("mailtrap");
+const Invitation = require('../models/invitation.model');
 
 const TOKEN = "06fd42045a4805b24b3a6321420a8ea0";
 const ENDPOINT = "https://send.api.mailtrap.io/";
@@ -39,26 +41,33 @@ router.post('/send-invite', async (req, res, next) => {
     try {
         email = req.body.email;
         role = req.body.role;
+        const doesExit = await User.findOne({ email })
+        if (doesExit) {
+            res.redirect('/auth/register')
+            return
+        }
+        const invitation = new Invitation(req.body);
+        await invitation.save()
         res.redirect('/auth/accept-invite');
-    } 
+    }
     catch (e) {
         res.redirect('/send-invite');
     }
     // try {
-        // email = req.body.email;
-        // role = req.body.role;
+    // email = req.body.email;
+    // role = req.body.role;
 
-        // client
-        //     .send({
-        //         from: sender,
-        //         to: recipients,
-        //         subject: "You are awesome!",
-        //         text: "Congrats for sending test email with Mailtrap!",
-        //         category: "Integration Test",
-        //     })
-        //     .then(console.log, console.error);
+    // client
+    //     .send({
+    //         from: sender,
+    //         to: recipients,
+    //         subject: "You are awesome!",
+    //         text: "Congrats for sending test email with Mailtrap!",
+    //         category: "Integration Test",
+    //     })
+    //     .then(console.log, console.error);
 
-        // Insert mail trap code here to send an email
+    // Insert mail trap code here to send an email
     // } catch (e) {
     //     next(e);
     // }
@@ -79,11 +88,19 @@ router.post('/register', async (req, res, next) => {
     try {
         // console.log(req.body)
         const email = req.body.email
-        const doesExit = await User.findOne({ email })
+        const doesExit = await User.findOne({ email });
+        const isInvited = await Invitation.findOne({ email });
+
         if (doesExit) {
             res.redirect('/auth/register')
             return
         }
+
+        if (isInvited) {
+            isInvited.invitationStatus = invitationStatus.accepted;
+            isInvited.save();
+        }
+
         const user = new User(req.body);
         await user.save()
         res.redirect('/auth/login')
